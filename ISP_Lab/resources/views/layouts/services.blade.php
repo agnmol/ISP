7c2b2961dfe9e6ee21eea7b4bbcbb97cfddc1254
agnmol <moleikaityte.agne@gmail.com>
2019-11-11 21:35:13 +0200

    services update

--- ISP_Lab/resources/views/layouts/services.blade.php
+++ ISP_Lab/resources/views/layouts/services.blade.php
@@ -3,9 +3,10 @@
         <h2>Kategorijos</h2>
         <ul>
             <li><a href="{{url('services')}}" class="{{Request::is('services') ? 'active' : ''}}">Visos paslaugos</a></li>
-            <li><a href="{{url('services/group1')}}" class="{{Request::is('services/group1') ? 'active' : ''}}">1 grupė</a></li>
-            <li><a href="{{url('services/group2')}}" class="{{Request::is('services/group2') ? 'active' : ''}}">2 grupė</a></li>
-
+            <ul>
+                <li><a href="{{url('services/group1')}}" class="{{Request::is('services/group1') ? 'active' : ''}}">1 grupė</a></li>
+                <li><a href="{{url('services/group2')}}" class="{{Request::is('services/group2') ? 'active' : ''}}">2 grupė</a></li>
+            </ul>
             <li><a href="{{url('services/add')}}" class="{{Request::is('services/add') ? 'active' : ''}}">Pridėti paslaugą</a></li>
             <li><a href="{{url('services/booked')}}" class="{{Request::is('services/booked') ? 'active' : ''}}">Užsakytos paslaugos</a></li>
         </ul>
