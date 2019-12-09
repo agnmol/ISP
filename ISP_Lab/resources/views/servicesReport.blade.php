4dae555e9d81f07f025b479112df8470486390e4
gedaus <gedis261@gmail.com>
2019-12-09 20:49:10 +0200

    Ataskaitos pataisymas

--- ISP_Lab/resources/views/servicesReport.blade.php
+++ ISP_Lab/resources/views/servicesReport.blade.php
@@ -15,7 +15,7 @@
                     <label for="parinktaDatanuo">Data nuo</label>
                 </div>
                 <div class="rightcol">
-                    <input type="datetime-local" id="parinktaDatanuo" name="parinktaDatanuo" min="<?php echo date('Y-m-d'."T".'H:i');?>">
+                    <input type="datetime-local" id="parinktaDatanuo" name="parinktaDatanuo">
                 </div>
             </div>
             <div class="row">
@@ -23,7 +23,7 @@
                     <label for="parinktaDataiki">Data iki</label>
                 </div>
                 <div class="rightcol">
-                    <input type="datetime-local" id="parinktaDataiki" name="parinktaDataiki" min="<?php echo date('Y-m-d'."T".'H:i');?>">
+                    <input type="datetime-local" id="parinktaDataiki" name="parinktaDataiki">
                 </div>
             </div>
             <div class="row">
@@ -33,7 +33,7 @@
                 <div class="rightcol">
                     <select class="field" name="grupe">
                         <?php
-                        echo "<option value=\"0\">Nesvarbu</option>";
+                        echo "<option selected=\"selected\" value=\"0\">Nesvarbu</option>";
                         while ($row = mysqli_fetch_assoc($grupes)){
                             echo "<option value=\"".$row['id']."\">".$row['pavadinimas']."</option>";
                         }
@@ -47,28 +47,27 @@
         </form>
         <br>
         <?php
-        if (isset($_GET['parinktaDatanuo']) && $_GET['parinktaDatanuo'] != null && $_GET['parinktaDataiki'] && $_GET['parinktaDataiki'] != null && $_GET['grupe']){
+        if (isset($_GET['parinktaDatanuo']) && $_GET['parinktaDatanuo'] != null && $_GET['parinktaDataiki'] && $_GET['parinktaDataiki'] != null && isset($_GET['grupe'])){
 
-            //$test = date("Y/m/d H:i:s", strtotime($_GET['parinktaDatanuo']));
-            //$parinktaDatanuo = new DateTime($test);
-            $parinktaDatanuo = $_GET['parinktaDatanuo'];
-            //$test = date("Y/m/d H:i:s", strtotime($_GET['parinktaDataiki']));
-            //$parinktaDataiki = new DateTime($test);
-            $parinktaDataiki = $_GET['parinktaDataiki'];
-
+            $parinktaDatanuo = date('Y-m-d h:i:s', strtotime($_GET['parinktaDatanuo']));
+            $parinktaDataiki = date('Y-m-d h:i:s', strtotime($_GET['parinktaDataiki']));
+            if ($parinktaDatanuo<$parinktaDataiki)
+                {
             $grupe = $_GET['grupe'];
-
-            $sql = "SELECT grupes.pavadinimas AS grupespav, paslaugos.pavadinimas, paslaugos.aprasymas, paslaugos.kaina, paslaugu_uzsakymai.komentaras,  paslaugu_uzsakymai.laikas, paslaugu_uzsakymai.vykdoma from paslaugu_uzsakymai
+            $i = 0;
+            $sql = "SELECT laikas, grupes.pavadinimas AS grupespav, paslaugos.pavadinimas, paslaugos.aprasymas, paslaugos.kaina, paslaugu_uzsakymai.komentaras,  paslaugu_uzsakymai.laikas, paslaugu_uzsakymai.vykdoma from paslaugu_uzsakymai
                     LEFT JOIN paslaugos ON paslaugos.id = paslaugu_uzsakymai.paslauga
-                    LEFT JOIN grupes ON paslaugos.grupe = grupes.id
-                    where laikas >= ".$parinktaDatanuo." AND laikas <= ".$parinktaDataiki;
+                    LEFT JOIN grupes ON paslaugos.grupe = grupes.id";
+                    //where laikas >= ".$parinktaDatanuo." AND laikas <= ".$parinktaDataiki."";
             if ($grupe != 0)
-                $sql = $sql." and grupes.id = ".$grupe;
+                $sql = $sql." where grupes.id = ".$grupe;
             $paslaugos = mysqli_query($db, $sql);
             echo "<table id=\"customers\">";
             $header = "<tr><th>Įvykdyta</th><th>Laikas</th><th>Komentaras</th><th>Grupe</th><th>Pavadinimas</th><th>Aprašymas</th><th>Kaina</th>";
             echo $header."</tr>";
-            /*while ($paslauga = mysqli_fetch_assoc($paslaugos)){
+            while ($paslauga = mysqli_fetch_assoc($paslaugos)){
+                if($paslauga['laikas']>=$parinktaDatanuo && $paslauga['laikas']<=$parinktaDataiki)
+                {
                 if ($paslauga['vykdoma']==1)
                 {
                     $meniuData = "<tr><td>Ne</td><td>";
@@ -82,9 +81,14 @@
                 $meniuData = $meniuData.$paslauga['grupespav']."</td><td>";
                 $meniuData = $meniuData.$paslauga['pavadinimas']."</td><td>";
                 $meniuData = $meniuData.$paslauga['aprasymas']."</td><td>".$paslauga['kaina']."</td>";
+                $i += $paslauga['kaina'];
                 echo $meniuData."</tr>";
-            }*/
+                }
+            }
             echo "</table>";
+            echo " <center><h1>Visa suma: $i</h1></center>";
+            }
+            else echo "Data nuo vėlesnė už datą iki";
         }
         else{
             echo "Ne visi filtravimo laukai užpildyti..";
